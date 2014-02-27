30efda3bb580119eece05f02b2ab959224201885
twc01327 <twc01327@gmail.com>
2014-02-27 22:05:53 +0800

    day1 close

--- simpleUI/src/com/twc01327/projectday1/simpleui/MainActivity.java
+++ simpleUI/src/com/twc01327/projectday1/simpleui/MainActivity.java
@@ -2,22 +2,68 @@ package com.twc01327.projectday1.simpleui;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private EditText editText;
+	private Button button;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		editText = (EditText) findViewById(R.id.editText1);
+		editText.setHint("type something....");
+		editText.setOnKeyListener(new OnKeyListener() {
 
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				Log.d("debug",
+						"keycode=" + keyCode + "keyEvent=" + event.getAction());
+				if (event.getAction() == KeyEvent.ACTION_DOWN){
+					if(keyCode == KeyEvent.KEYCODE_ENTER){
+						sendMessage();
+						return true;
+					}
+				}
+				return false;
+			}
+		});
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		button = (Button) findViewById(R.id.button1);
+		button.setText("send");
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				sendMessage();
+			}
+		});
+
+	}
+
+	public void submit2(View v) {
+		sendMessage();
+	}
+
+	public void sendMessage() {
+		String text = editText.getText().toString();
+		editText.getText().clear();
+		Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
---
+++ simpleUIcontrol/gen/com/twc01327/projectday1/simpleuicontrol/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.twc01327.projectday1.simpleuicontrol;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
